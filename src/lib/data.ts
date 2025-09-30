0397322a386aa28cb31b91a4859efe485ad97417
AndresMM1 <andres.moram@utadeo.edu.co>
2025-09-30 15:09:47 -0500

    Description fix

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -95,7 +95,7 @@ export async function getIncidents(): Promise<Incident[]> {
 export async function addIncident(incident: Omit<Incident, 'id' | 'status' | 'endDate'>, userEmail: string): Promise<Incident> {
   const apiPayload = {
     AFFECT_STATE: 'Proceso',
-    AFFECT_DETAILS: `Servicio: ${incident.service} Descripción: ${incident.description}`,
+    AFFECT_DETAILS: incident.description,
     AFFECT_START_DATE: incident.startTime,
     PERSON_EMAIL: userEmail,
     AFFECT_PRIORITY: incident.priority,
